200e72bb5acac1dd26357631ac3681c684430f6b
Ces1a <cesiapajaroc@hotmail.com>
2024-10-31 17:47:31 -0500

    new add

--- Proyectos/portafolio/src/pages/Experience.jsx
+++ Proyectos/portafolio/src/pages/Experience.jsx
@@ -7,12 +7,33 @@ export const Experience = () => {
         <div className='project'>
             <div className='cuadrado'>
                 <h1>job</h1>
+                <ul className="list">
+                    <h3>free time</h3>
+                    <li className='lista'>Agrega texto</li>
+                    <li className='lista'>Agrega texto</li>
+                    <li className='lista'>Agrega texto</li>
+                    <li className='lista'>Agrega texto</li>
+                </ul>
             </div>
             <div className='cuadrado'>
                 <h1>academico</h1>
+                <ul className="list">
+                    <h3>free time</h3>
+                    <li className='lista'>Agrega texto</li>
+                    <li className='lista'>Agrega texto</li>
+                    <li className='lista'>Agrega texto</li>
+                    <li className='lista'>Agrega texto</li>
+                </ul>
             </div>
             <div className='rectangulo'>
                 <h1>habilidades</h1>
+                <ul className="list">
+                    <h3>free time</h3>
+                    <li className='lista'>Agrega texto</li>
+                    <li className='lista'>Agrega texto</li>
+                    <li className='lista'>Agrega texto</li>
+                    <li className='lista'>Agrega texto</li>
+                </ul>
             </div>
         </div>
     )
